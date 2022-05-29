54e27b792a7033933b140cfc123e36ab9a6376ae
Bea <34965770+Beacouchoud@users.noreply.github.com>
2022-05-29 18:57:35 +0200

    fix: fixed search url

--- src/services/utils.js
+++ src/services/utils.js
@@ -3,7 +3,7 @@ export const LANG_ES = "&language=es-ES";
 export const API_URL = "https://api.themoviedb.org/3/";
 export const BASE_MOVIE_URL = API_URL+"movie/";
 export const BASE_SERIE_URL = API_URL+"tv/";
-export const BASE_SEARCH_MOVIE_URL = API_URL+"search/movie?"+API_KEY+LANG_ES+"&query=";
+export const BASE_SEARCH_MOVIE_URL = API_URL+"search/movie"+API_KEY+LANG_ES+"&query=";
 export const BASE_SEARCH_SERIE_URL = API_URL+"search/movie";
 export const POPULAR_MOVIES_URL = API_URL+ "movie/popular"+API_KEY+LANG_ES;
 export const BASE_IMG_URL = "https://image.tmdb.org/t/p/w500"
