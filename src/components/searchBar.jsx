8e5b61089cbe9ecf36dfe3b3b3f5a074a7bf0946
Bea <34965770+Beacouchoud@users.noreply.github.com>
2022-05-30 03:35:33 +0200

    feat: enabled search by genre

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,36 +1,76 @@
-import React from "react";
-import { BASE_SEARCH_MOVIE_URL } from "../services/utils";
+import React, { useEffect, useState } from "react";
+import { BASE_SEARCH_MOVIE_URL, LATEST_MOVIES_URL } from "../services/utils";
+import { GenreSelector } from "./genreSelector";
+import "../static/styles/forms.css";
 
-export const SearchBar = ({ setSearchTerm, searchTerm, setIsLoading, setMovies }) => {
-  const searchMovies = () => {
+export const SearchBar = ({ setSearchTerm, searchTerm, setIsLoading, setMovies, setShowMessage }) => {
+  const [genre, setGenre] = useState(0);
+
+  const searchMovies = async () => {
     setIsLoading(true);
-    fetch(BASE_SEARCH_MOVIE_URL + searchTerm)
-      .then((response) => response.json())
-      .then((movies) => {
-        console.log(movies.results);
-        setMovies(movies.results);
-        setTimeout(() => {
-          setIsLoading(false);
-        }, 1000);
-      });
+    let moviesList = [];
+    let response;
+    if (searchTerm === "") {
+      let page = 0;
+      let totalPages;
+      do {
+        page++;
+        response = await fetch(LATEST_MOVIES_URL + "&page=" + page);
+        const moviesResult = await response.json();
+        totalPages = moviesResult.total_pages;
+        if (genre === 0 || genre === "0") {
+          moviesList.push(...moviesResult.results);
+        } else {
+          console.log(moviesResult.results.filter((movie) => movie.genre_ids.includes(+genre)));
+          moviesList.push(...moviesResult.results.filter((movie) => movie.genre_ids.includes(+genre)));
+        }
+      } while (page < totalPages);
+    } else {
+      let page = 1;
+      while (moviesList?.length < 20) {
+        if (page > 25) break;
+        response = await fetch(BASE_SEARCH_MOVIE_URL + searchTerm + "&page=" + page);
+        const moviesResult = await response.json();
+
+        if (+genre === 0) moviesList.push(...moviesResult.results);
+        else moviesList.push(...moviesResult.results.filter((movie) => movie.genre_ids.includes(+genre)));
+        page++;
+      }
+    }
+
+    setMovies(moviesList);
+    setIsLoading(false);
+    setShowMessage(false);
   };
   const handleForm = (ev) => {
     ev.preventDefault();
     searchMovies();
   };
 
+  useEffect(() => {
+    console.log(genre);
+    searchMovies();
+  }, [genre]);
+
   return (
-    <form class="d-flex" onSubmit={handleForm}>
-      <input
-        class="form-control form-control-lg"
-        type="search"
-        placeholder="Search"
-        aria-label="Search"
-        onChange={(ev) => setSearchTerm(ev.target.value)}
-      />
-      <button class="btn btn-outline-info" type="button" onClick={searchMovies}>
-        Buscar
-      </button>
+    <form className="d-flex" onSubmit={handleForm}>
+      <div className="input-group">
+        <span>
+          <GenreSelector setGenre={setGenre} className="flex-shrink-1 flex-grow-0 "></GenreSelector>
+        </span>
+        <input
+          className="form-control form-control-lg flex-grow-1"
+          type="search"
+          placeholder="Buscar..."
+          aria-label="Search"
+          onChange={(ev) => setSearchTerm(ev.target.value)}
+        />
+        <span>
+          <button className="btn btn-outline-info btn-lg" type="button" onClick={searchMovies}>
+            Buscar
+          </button>
+        </span>
+      </div>
     </form>
   );
 };
--- src/pages/searchPage.jsx
+++ src/pages/searchPage.jsx
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import { Header } from "../components/header";
 import { MovieCard } from "../components/movieCard";
 import { SearchBar } from "../components/searchBar";
 import { Spinner } from "../components/spinner";
+import { LATEST_MOVIES_URL } from "../services/utils";
 
 export const SearchPage = ({}) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [movies, setMovies] = useState();
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(LATEST_MOVIES_URL)
+      .then((response) => response.json())
+      .then((moviesList) => {
+        setMovies(moviesList.results);
+        setTimeout(() => {
+          setIsLoading(false);
+          setShowMessage(true);
+        }, 1000);
+      });
+  }, []);
+
+  const vaciar =()=> {
+    debugger;
+    setMovies(null);
+  }
+
   return (
     <>
       <div className="container-fluid">
@@ -16,39 +36,34 @@ export const SearchPage = ({}) => {
           <Header></Header>
         </div>
         <div className="row my-5">
-          <div className="col-2 m-auto">
-            <Link to="/popular" className="btn btn-lg btn-info text-light">
-              Top 10 películas
-            </Link>
-          </div>
           <div className="col-6 m-auto">
-            <SearchBar 
-                setSearchTerm={setSearchTerm} 
-                searchTerm={searchTerm} 
-                setIsLoading={setIsLoading} 
-                setMovies={setMovies}>       
-            </SearchBar>
+            <SearchBar
+              setSearchTerm={setSearchTerm}
+              searchTerm={searchTerm}
+              setIsLoading={setIsLoading}
+              setMovies={setMovies}
+              vaciar={vaciar}
+              movies={movies}
+              setShowMessage={setShowMessage}
+            ></SearchBar>
           </div>
-          <div className="col-2 m-auto">
-            <select class="form-select form-select-lg" defaultValue={0} aria-label=".form-select-lg example">
-              <option value="0">Todos los géneros</option>
-              <option value="1">Romantica</option>
-              <option value="2">Drama</option>
-              <option value="3">Comedia</option>
-            </select>
-          </div>
+
         </div>
         <div className="row">
-            <div className="col-8 m-auto">
+          <div className="col-8 m-auto">
             {isLoading && <Spinner></Spinner>}
+
             {movies && (
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 d-flex justify-content-evenly">
-              {movies.map((movie, index) => (
-                <MovieCard class="col" movie={movie}></MovieCard>
-              ))}
-            </div>
-          )}
-            </div>
+              <>
+                {showMessage && <h3 className="mb-5">Ahora en cines</h3>}
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 d-flex justify-content-evenly">
+                  {movies.map((movie, index) => (
+                    <MovieCard className="col" movie={movie} key={index}></MovieCard>
+                  ))}
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </>
