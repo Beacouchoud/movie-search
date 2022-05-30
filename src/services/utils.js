1b205d11063f923b05391d2f87fc75161be02e70
Bea <34965770+Beacouchoud@users.noreply.github.com>
2022-05-30 03:34:13 +0200

    chore: added url constants

--- src/services/utils.js
+++ src/services/utils.js
@@ -5,5 +5,7 @@ export const BASE_MOVIE_URL = API_URL+"movie/";
 export const BASE_SERIE_URL = API_URL+"tv/";
 export const BASE_SEARCH_MOVIE_URL = API_URL+"search/movie"+API_KEY+LANG_ES+"&query=";
 export const BASE_SEARCH_SERIE_URL = API_URL+"search/movie";
+export const LATEST_MOVIES_URL = API_URL+"movie/now_playing"+API_KEY+LANG_ES+"&region=ES";
 export const POPULAR_MOVIES_URL = API_URL+ "movie/popular"+API_KEY+LANG_ES;
 export const BASE_IMG_URL = "https://image.tmdb.org/t/p/w500"
+export const GET_GENRES_URL = API_URL+"genre/movie/list"+API_KEY+LANG_ES;
