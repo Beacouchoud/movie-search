b5167eb0bb7c7aa410e99aa5fba78ebc2ea4cd78
Bea <34965770+Beacouchoud@users.noreply.github.com>
2022-05-30 19:21:40 +0200

    style: header look and feel

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,16 +7,13 @@ export const Header = ({ movie }) => {
   return (
     <nav className="navbar fixed-top navbar-light bg-light col-12 ">
       <div className="container-fluid">
-        <Link to="/" className="navbar-brand text-info ">
+        <Link to="/" className="navbar-brand text-info fs-3">
           <FontAwesomeIcon icon={faVideo} className="me-2" />
           Buscador de películas
         </Link>
         <div className="col-3 ms-auto me-0 text-end">
-          <Link to="/popular" className="btn btn-lg btn-info text-light mx-4">
+          <Link to="/popular" className="btn btn-lg btn-info text-light">
             Top 10 películas
-          </Link>
-          <Link to="/" className="btn btn-outline-info btn-lg text-dark ">
-            Series
           </Link>
         </div>
       </div>
